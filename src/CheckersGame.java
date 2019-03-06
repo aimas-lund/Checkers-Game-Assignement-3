1ceed3a0feeb6258fb0286680cdebba0ebd907ce
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2019-03-06 16:30:42 +0100

    Turn switch implemented

--- src/CheckersGame.java
+++ src/CheckersGame.java
@@ -1,6 +1,22 @@
+
+
 public class CheckersGame {
 
+    public void switchTurn(Player p1, Player p2) {
+        System.out.println("\nSwitching Turn!");
+        if (p1.isTurn()) {
+            p1.setTurn(false);
+            p2.setTurn(true);
+            System.out.println("Turn of: Player " + p2.getPlayerNumber());
+        } else {
+            p1.setTurn(true);
+            p2.setTurn(false);
+            System.out.println("Turn of: Player " + p1.getPlayerNumber());
+        }
+    }
+
     public static void main(String[] args) {
+        CheckersGame game = new CheckersGame();
         System.out.println("Welcome to Object-oriented Checkers!");
 
         System.out.println("Initializing game...\n");
@@ -10,5 +26,8 @@ public class CheckersGame {
 
         board.initBoard(player1, player2);
         board.printBoard();
+        player1.setTurn(true);
+        game.switchTurn(player1, player2);
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -14,14 +14,17 @@ enum Players {
     public void setNumber(int number) {
         this.number = number;
     }
+
 }
 
 public class Player {
 
     private Players pn;
+    private boolean turn;
 
     public Player(Players pn) {
         this.pn = pn;
+        this.turn = false;
     }
 
     public int getPlayerNumber() {
@@ -30,5 +33,13 @@ public class Player {
 
     public void printPlayerNumber() {
         System.out.println("Player " + getPlayerNumber());
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
+
+    public boolean isTurn() {
+        return this.turn;
     }
 }
